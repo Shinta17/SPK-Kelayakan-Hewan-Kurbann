0889c096b29c3836d4c708b232e261694a1ae506
Shinta17 <147657274+Shinta17@users.noreply.github.com>
2026-07-07 19:01:04 +0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+# Program penjumlahan dua angka
+
+angka1 = int(input("Masukkan angka pertama: "))
+angka2 = int(input("Masukkan angka kedua: "))
+
+hasil = angka1 + angka2
+
+print("Hasil penjumlahan =", hasil)
